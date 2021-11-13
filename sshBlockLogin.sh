7ab156e1e7644dd8339d826f440fb37b96191d5e
kxisxr <47679627+k4is3r13@users.noreply.github.com>
2021-11-13 16:45:41 -0600

    Create sshBlockLogin.sh

---
+++ sshBlockLogin.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+#Author: kxisxr
+greenColour="\x1B[0;32m\033[1m"
+endColour="\033[0m\x1B[0m"
+redColour="\x1B[0;31m\033[1m"
+blueColour="\x1B[0;34m\033[1m"
+yellowColour="\x1B[0;33m\033[1m"
+purpleColour="\x1B[0;35m\033[1m"
+turquoiseColour="\x1B[0;36m\033[1m"
+grayColour="\x1B[0;37m\033[1m"
+
+if [[ $(/usr/bin/id -u) -ne 0 ]]; then
+    echo -e -n "${redColour}"'Not running as root \nExiting...'"${endColour}"
+    exit
+fi
+
+echo -e "${greenColour}"'1.- Enable password login.'"${endColour}"
+echo -e "${redColour}"'2.- Disable password login.\n'"${endColour}"
+
+echo -e -n "${yellowColour}"'Selecciona una opcion: '"${endColour}"
+read -e value
+
+if [ $value == '1' ]
+then
+sed 's/ChallengeResponseAuthentication no/ChallengeResponseAuthentication yes/g' /etc/ssh/sshd_config | sponge /etc/ssh/sshd_config
+
+sed 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config | sponge /etc/ssh/sshd_config
+
+sed 's/UsePAM no/UsePAM yes/g' /etc/ssh/sshd_config | sponge /etc/ssh/sshd_config
+
+sed 's/PermitRootLogin no/#PermitRootLogin prohibit-password/g' /etc/ssh/sshd_config | sponge /etc/ssh/sshd_config
+
+echo -e ' '
+sleep 0.5
+echo -e "${turquoiseColour}"'Reloading /etc/init.d/ssh service.\n'"${endColour}"
+/etc/init.d/ssh reload > /dev/null 2>&1
+sleep 0.5
+echo -e ' '
+
+else
+
+sed 's/ChallengeResponseAuthentication yes/ChallengeResponseAuthentication no/g' /etc/ssh/sshd_config | sponge /etc/ssh/sshd_config
+
+sed 's/PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config | sponge /etc/ssh/sshd_config
+
+sed 's/UsePAM yes/UsePAM no/g' /etc/ssh/sshd_config | sponge /etc/ssh/sshd_config
+
+sed 's/#PermitRootLogin prohibit-password/PermitRootLogin no/g' /etc/ssh/sshd_config | sponge /etc/ssh/sshd_config
+
+echo -e ' '
+sleep 0.5
+echo -e "${turquoiseColour}"'Reloading /etc/init.d/ssh service.\n'"${endColour}"
+/etc/init.d/ssh reload > /dev/null 2>&1
+sleep 0.5
+echo -e ' '
+
+fi
